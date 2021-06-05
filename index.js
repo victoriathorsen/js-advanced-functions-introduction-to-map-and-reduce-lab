54ae2cecbf416fdd4e2849c2043c20cc328b50c4
Victoria <victoriathorsenn@gmail.com>
2021-06-05 00:16:07 -0400

    commit tree f7781f849525bec867383bdc1a1d50d89a06d66a

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+const mapToNegativize = (array) => {
+    return array.map(number => (number * -1))
+}
+
+const mapToNoChange = (array) => {
+    return array
+}
+
+const mapToDouble = (array) => {
+    return array.map(number => (number * 2))
+}
+
+const mapToSquare = (array) => {
+    return array.map(number => (number ** 2))
+}
+
+function reduceToTotal(src, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < src.length; i++ ) {
+      total = total + src[i]
+    }
+    return total
+  }
+  
+  function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+  }
+  
+  function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
+  }
